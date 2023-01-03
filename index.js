3e14f0f6249de9bf94efcb0d1e137f1c92ecdbdc
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-01-02 21:03:33 -0400

    Create Player with Event Listeners
    Create Player with Event Listeners

--- index.js
+++ index.js
@@ -8,26 +8,38 @@ canvas.height = 576
 // Draw the board 
 c.fillRect(0,0,canvas.width, canvas.height);
 
+const gravity = 0.2
+
 class Sprite 
 {
     constructor({ position, velocity })
     {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     // Create a default player and fill the rect to red color
     draw()
     {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
 
     }
 
     //Create a update
     update()
     {
-        
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if(this.position.y + this.height + this.velocity.y >= canvas.height)
+        {
+            this.velocity.y = 0
+        } else
+        {
+            this.velocity.y += gravity
+        }
     }
 
 }
@@ -66,6 +78,10 @@ enemy.draw()
 function animate()
 {
     window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0,0,canvas.width, canvas.height)
+    player.update()
+    enemy.update()
 }
 
 animate()
