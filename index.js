f2287b28977f2df944176ff4713853dc83e27670
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-01-27 22:53:09 -0400

    detect for collision

--- index.js
+++ index.js
@@ -180,12 +180,7 @@ function animate()
     }
 
     // detect for collision
-    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-        player.attackBox.position.x <= enemy.position.x + enemy.width &&
-        player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-        player.attackBox.position.y <= enemy.position.y + enemy.height &&
-        player.isAttacking
-        )
+    if (rectangularCollision({rectangle1:player, rectangle2:enemy}) && player.isAttacking)
     {
         player.isAttacking = false
         console.log("go")
